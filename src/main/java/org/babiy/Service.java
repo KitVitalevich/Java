1127df962f09d00ea783c49a315ca06aeceec5b0
KitVitalevich <kit.vitalevich@ya.ru>
2024-02-21 10:06:02 +0700

    Created the info() method in the Service class

--- src/main/java/org/babiy/Service.java
+++ src/main/java/org/babiy/Service.java
@@ -1,4 +1,15 @@
 package org.babiy;
 
 public class Service {
+    public void info() {
+        System.out.println("Service");
+    }
+    
+    public static void main(String[] args) {
+        Service service = new Service();
+        service.info();
+    }
+
+
+
 }
